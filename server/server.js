03f218ef1ea849b81128990870a9df3b64a231de
kelar crisp <kelcrisp007@icloud.com>
2020-06-09 19:21:11 -0700

    full client and server setup

---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+require("dotenv").config();
+const app = express();
+
+app.use(morgan("tiny"));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+const mongoDbUri = mongoose.connect(
+  `mongodb+srv://kelar:${process.env.DB_PASSWORD}@store-o4whr.mongodb.net/store?retryWrites=true&w=majority
+`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoose is connected");
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`running on port ${process.env.PORT}`);
+});
+
+module.exports = app;
