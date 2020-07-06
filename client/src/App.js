49e19091dcd5df598a49c783723e1ff115c396a1
kelar crisp <kelcrisp007@icloud.com>
2020-07-06 11:07:08 -0700

    adding suspense for the fallback and added logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import "./App.css";
-import Login from "./login/Login";
+
 import Products from "./products/Products";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { getJwt } from "./helperfunctions/getjwt";
@@ -8,31 +8,35 @@ import NavBar from "./navbar/NavBar";
 import Checkout from "./checkout/Checkout";
 import { ProductContextProvider } from "./context/ProductContext";
 import IndividualProductComponent from "./products/individualProductComponent/IndividualProductComponent";
+import LoadingPlaceholder from "./loadingPlaceholder/LoadingPlaceholder";
+const Login = lazy(() => import("./login/Login"));
 function App() {
   return (
-    <BrowserRouter>
-      <>
-        <Switch>
-          <ProductContextProvider>
-            <Route path="/signin" exact component={Login} />
-            <Route
-              path="/products/:id"
-              component={IndividualProductComponent}
-            />
-            <Route path="/products" exact component={Products} />
+    <Suspense fallback={LoadingPlaceholder}>
+      <BrowserRouter>
+        <>
+          <Switch>
+            <ProductContextProvider>
+              <Route path="/signin" exact component={Login} />
+              <Route
+                path="/products/:id"
+                component={IndividualProductComponent}
+              />
+              <Route path="/products" exact component={Products} />
 
-            <Route path="/checkout" exact component={Checkout} />
-            <Route
-              exact
-              path="/"
-              render={() =>
-                !getJwt() ? <Redirect to="/signin" /> : <Products />
-              }
-            />
-          </ProductContextProvider>
-        </Switch>
-      </>
-    </BrowserRouter>
+              <Route path="/checkout" exact component={Checkout} />
+              <Route
+                exact
+                path="/"
+                render={() =>
+                  !getJwt() ? <Redirect to="/signin" /> : <Products />
+                }
+              />
+            </ProductContextProvider>
+          </Switch>
+        </>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
---
+++ client/src/loadingPlaceholder/LoadingPlaceholder.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LoadingPlaceholder = () => {
+  return (
+    <div className="container">
+      <div className="placeHolderContainer">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingPlaceholder;
